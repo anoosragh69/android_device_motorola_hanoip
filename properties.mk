2df6bc406ef3d700d3d5f8aa694aacba45502b3b
Vasantha Balla <vballa@codeaurora.org>
2019-09-24 12:39:06 +0530

    hanoip: Add property to prefer hw codecs in thumbnail
    Add property "media.stagefright.thumbnail.prefer_hw_codecs"
    to system.prop
    
    Change-Id: If60f9cdc8b0236df421a72e7621f1e57fd3f4f55
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- properties.mk
+++ properties.mk
@@ -261,6 +261,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
     persist.vendor.media.recorder.bt709=true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
