ddf3ef74d77b5ced091c50d3d949c43392b384ab
John Galt <johngaltfirstrun@gmail.com>
2023-11-27 11:00:43 -0500

    hanoip: improve lmkd behavior
    Thrashing and partial stall detection is far too aggressive.
    
    200/300 for normal and critical thrashing detection has been shown to be
    superior on our device.
    
    Full/critical limits are still best when running OOM test or triggering
    thrashing with cat.
    
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- properties.mk
+++ properties.mk
@@ -244,9 +244,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # LMKd
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lmk.thrashing_limit=30 \
+    ro.lmk.thrashing_limit=200 \
     ro.lmk.swap_free_low_percentage=10 \
-    ro.lmk.psi_partial_stall_ms=200 \
+    ro.lmk.psi_partial_stall_ms=120 \
     ro.lmk.swap_util_max=100 \
     ro.lmk.threshold_decay=30 \
     ro.lmk.thrashing_limit_decay=50 \
