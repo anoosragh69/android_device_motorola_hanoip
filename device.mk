04e5e6939a46ea8beea012b9b298400c3102fa83
Alexander Koskovich <zvnexus@outlook.com>
2024-03-04 10:21:28 +0530

    hanoip: Disable the usage of ConfigStore.
     * As per commit LineageOS/android_hardware_interfaces@11a113a ('Add disable_configstore').
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- device.mk
+++ device.mk
@@ -182,6 +182,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
 
+# ConfigStore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
