75a31b3cd88e58be88c204725892cec7cb885e3b
Roger Liao <rogerliao@google.com>
2024-03-04 11:38:46 +0530

    hanoip: Build vndservicemanager
    vndservicemanager has been removed for the devices with API level
    >= 30 but hanoip still needs it to provide vendor-implemented services.
    
    Ref: b/149867896
    Bug: 148823278
    Change-Id: I08ed6a242c8ac1f64da12204d618376fe1a5c8ac
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- device.mk
+++ device.mk
@@ -487,6 +487,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+# vndservicemanager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
