e6e42161eedea23a342cec529f5024c7bda0879f
anoosragh69 <ok.ano0s6090@gmail.com>
2024-04-03 17:49:05 +0530

    Partially revert "hanoip: Set PRODUCT_SHIPPING_API_LEVEL to 30"
    This reverts commit 31fdaafb6c4c9646c0c8843e81154c2256d0acb6.

--- device.mk
+++ device.mk
@@ -186,6 +186,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CameraGo
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
+
 # ConfigStore
 PRODUCT_PACKAGES += \
     disable_configstore
@@ -457,6 +460,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so 
 
 # QMI
 PRODUCT_PACKAGES += \
