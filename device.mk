4c0ec085e11d775dea38bb9805b79ec2fd9992a9
anoosragh69 <ok.ano0s6090@gmail.com>
2024-04-13 11:59:58 +0530

    Revert "sm6150-common: bootctrl: Remove bootctrl.sm6150 target"
    This reverts commit 08cfe79839a0455e9f7af8dce389c522f156e2fc.

--- device.mk
+++ device.mk
@@ -161,7 +161,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
     android.hardware.boot@1.1-service \
-    android.hardware.boot@1.1-impl-qti.recovery
+    android.hardware.boot@1.1-impl-qti.recovery \
+    bootctrl.sm6150 \
+    bootctrl.sm6150.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
