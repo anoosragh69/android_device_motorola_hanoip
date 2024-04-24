169139c1eb55099639c7aea16aea1f8682c7e021
anoosragh69 <ok.ano0s6090@gmail.com>
2024-04-24 18:17:54 +0530

    hanoip: Import MotoActions - from: https://github.com/anoosragh69/packages_apps_MotoActions
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

---
+++ MotoActions/Android.mk
@@ -0,0 +1,36 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := MotoActions
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_USE_AAPT2 := true
+
+LOCAL_OPTIONAL_USES_LIBRARIES := \
+    androidx.window.extensions \
+    androidx.window.sidecar
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    androidx.core_core \
+    android-support-v13 \
+    androidx.preference_preference
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    $(TOP)/packages/resources/devicesettings/res
+
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_DEX_PREOPT := true
+
+include frameworks/base/packages/SettingsLib/common.mk
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
