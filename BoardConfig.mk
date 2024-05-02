7d44cb2012260c7f3e90cf074e096f02f94c978e
anoosragh69 <ok.ano0s6090@gmail.com>
2024-05-02 20:03:09 +0530

    Revert "sm6150-common: Migrate to restructured sepolicy_vndr"
    This reverts commit 9b8303e26c7c5ed014b61d62d7ad58f8a1af4867.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,7 +198,7 @@ VENDOR_SECURITY_PATCH := 2023-07-01
 
 # SELinux
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
