6e18c79900431b35279622736023b1cf2526761e
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:46:19 +0300

    hanoip: Include lineage FCM
    Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,7 +140,9 @@ USE_DEVICE_SPECIFIC_GPS := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 
