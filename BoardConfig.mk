f3cac405bd5634966cf58b576e0c0cc134975268
Michael Bestas <mkbestas@lineageos.org>
2024-06-17 10:41:47 +0300

    hanoip: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,6 +141,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
