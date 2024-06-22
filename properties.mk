3fb6cfae7ea2fd427095aa4e23bc4bac41f8a6aa
Matt Buckley <mattbuckley@google.com>
2024-06-22 12:13:13 +0530

    hanoip: Enable HWUI adpf
    Bug: b/261628396
    Test: manual
    Change-Id: I7352f933993770498d3403aeb4e45fd3f1afab88
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- properties.mk
+++ properties.mk
@@ -210,6 +210,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.sf.native_mode=1 \
     persist.sys.sf.force_brightness_capability=1 \
     debug.sf.disable_client_composition_cache=1 \
+    debug.sf.enable_adpf_cpu_hint=true \
     ro.opengles.version=196610 \
     ro.gfx.driver.1=com.qualcomm.qti.gpudrivers.sm6150.api30 \
     vendor.display.enable_default_color_mode=1 \
