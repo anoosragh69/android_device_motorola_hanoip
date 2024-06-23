3ce74543d2e4584aef4c8c1b01db53b2fd41162e
Michael Bestas <mkbestas@lineageos.org>
2024-06-24 00:54:19 +0300

    hanoip: Convert expat symlink to install_symlink target
    Change-Id: I4cf144e660d44bd0f28e5b94d9995ddd4c08b38c
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- device.mk
+++ device.mk
@@ -73,6 +73,10 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# OEM
+PRODUCT_PACKAGES += \
+    bin_expat_symlink
+
 # Properties
 -include $(LOCAL_PATH)/properties.mk
 
