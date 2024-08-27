f6bc3f3ff20d7c69fe5990915d9664022898b5d0
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:51:30 +0300

    hanoip: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -31,7 +31,7 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 write_headers
 
 # The standard device blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+write_makefiles "${MY_DIR}/proprietary-files.txt"
 
 # Finish
 write_footers
