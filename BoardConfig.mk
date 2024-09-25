cb843120570315b530b1e952077147cf96eec930
Bruno Martins <bgcngm@gmail.com>
2024-09-25 22:34:02 +0530

    hanoip: Use the common device compatibility matrix from QCOM
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,7 +139,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Init
