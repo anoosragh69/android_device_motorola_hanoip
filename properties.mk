c441cdeec22cf681b1a6c9c4fcb4bd73e3a520c4
Pranav Vashi <neobuddy89@gmail.com>
2024-12-03 17:00:49 +0530

    hanoip: Remove unused QCOM system daemon prop
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- properties.mk
+++ properties.mk
@@ -307,10 +307,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.qti.sys.fw.bg_apps_limit=60
 
-# Qualcomm System Daemon
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.qcomsysd.enabled=1
-
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.vendorprefix=/vendor \
