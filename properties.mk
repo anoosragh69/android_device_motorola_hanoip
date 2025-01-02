9ba4f1769047ae6f3532118882d9275f5bd08d59
DhineshCool <dhineshcool585@gmail.com>
2025-01-02 23:17:10 +0530

    hanoip: update props and tune powerhint
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- properties.mk
+++ properties.mk
@@ -205,13 +205,17 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.latch_unsignaled=1 \
+    debug.egl.hw=0 \
+    debug.mdpcomp.logs=0 \
+    debug.sf.hw=0 \
+    debug.sf.auto_latch_unsignaled=0 \
     persist.demo.hdmirotationlock=false \
     persist.sys.sf.color_saturation=1.0 \
     persist.sys.sf.native_mode=1 \
     persist.sys.sf.force_brightness_capability=1 \
     debug.sf.disable_client_composition_cache=1 \
     debug.sf.enable_adpf_cpu_hint=true \
+    debug.sf.latch_unsignaled=1 \
     ro.opengles.version=196610 \
     ro.gfx.driver.1=com.qualcomm.qti.gpudrivers.sm6150.api30 \
     vendor.display.enable_default_color_mode=1 \
