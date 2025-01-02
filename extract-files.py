66e1395f44139e79317d686b556d058268ee6883
luk1337 <priv.luk@gmail.com>
2025-01-02 17:30:12 +0530

    hanoip:Switch to py extract utils
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'hardware/qcom-caf/sm8150',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/etc/permissions/moto-telephony.xml': blob_fixup()
+        .regex_replace('system/framework', 'system/system_ext/framework'),
+    ('product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml', 'product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml'): blob_fixup()
+        .regex_replace('xml version="2.0"', 'xml version="1.0"'),
+    ('vendor/lib64/camera/components/com.qti.node.gpu.so', 'vendor/lib64/hw/com.qti.chi.override.so', 'vendor/bin/thermal-engine', 'vendor/bin/rmt_storage', 'vendor/lib64/libril-qc-hal-qmi.so', 'vendor/lib64/hw/camera.qcom.so'): blob_fixup()
+        .binary_regex_replace(b'camera.mot.is.coming.cts', b'vendor.camera.coming.cts'),
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'hanoip',
+    'motorola',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    add_firmware_proprietary_file=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
