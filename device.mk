ed50c8c5f69f25533abdf216626569413aae71db
DhineshCool <dhineshcool585@gmail.com>
2025-01-18 17:03:31 +0530

    hanoip: partially revert Remove OMX audio codecs
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- device.mk
+++ device.mk
@@ -376,10 +376,6 @@ PRODUCT_PACKAGES += \
     libchrome \
     libchrome.vendor
 
-PRODUCT_PACKAGES += \
-    libcodec2_hidl@1.0.vendor \
-    libcodec2_vndk.vendor
-
 # Moto hardware
 PRODUCT_PACKAGES += \
     MotoActions \
@@ -415,6 +411,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+
+# OMX
+PRODUCT_PACKAGES += \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxVidcCommon \
+    libstagefrighthw
 
 # Power
 PRODUCT_PACKAGES += \
